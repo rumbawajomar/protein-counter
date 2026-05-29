28d7c1eace9d0a28386551f8db792e8d724d27a8
rumbawajomar <rumbawajomar@gmail.com>
2026-05-29 14:46:27 -0700

    Update FoodEntry styles

--- src/components/FoodEntry.jsx
+++ src/components/FoodEntry.jsx
@@ -1,9 +1,22 @@
 export default function FoodEntry() {
   return (
-    <div>
-      <p>log a food</p>
-      <input type="text" placeholder="Food name (e.g. chicken breast)" />
-      <input type="number" placeholder="Protein (g)" />
+    <div className="px-6 py-4 bg-blue-500 rounded-lg text-white">
+      <p className="uppercase text-sm mb-2">log a food</p>
+      <input
+        className="w-full block outline outline-solid outline-white p-2 rounded-sm mb-4"
+        type="text"
+        placeholder="Food name (e.g. chicken breast)"
+      />
+      <div className="flex justify-between gap-4">
+        <input
+          className="outline outline-solid outline-white p-2 rounded-sm flex-2"
+          type="number"
+          placeholder="Protein (g)"
+        />
+        <button className="bg-green-600 text-white flex-1 rounded-sm">
+          Add +
+        </button>
+      </div>
     </div>
   )
 }
