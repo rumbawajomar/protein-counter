46de75eceb98a56e7261d7fecd774c7033dee777
rumbawajomar <rumbawajomar@gmail.com>
2026-05-29 09:40:56 -0700

    Update components

--- src/components/FoodEntry.jsx
+++ src/components/FoodEntry.jsx
@@ -1,3 +1,9 @@
 export default function FoodEntry() {
-  return <div>food entry component goes here</div>
+  return (
+    <div>
+      <p>log a food</p>
+      <input type="text" placeholder="Food name (e.g. chicken breast)" />
+      <input type="number" placeholder="Protein (g)" />
+    </div>
+  )
 }
--- src/components/FoodLog.jsx
+++ src/components/FoodLog.jsx
@@ -1,3 +1,27 @@
 export default function FoodLog() {
-  return <div>foodlog component goes here</div>
+  return (
+    <div>
+      <p>Today's Log</p>
+      <ul>
+        <li>
+          <p>Chicken Breast</p>
+          <time>8:32am</time>
+          <p>32g</p>
+          <button>delete</button>
+        </li>
+        <li>
+          <p>Greek yogurt</p>
+          <time>10:15am</time>
+          <p>17g</p>
+          <button>delete</button>
+        </li>
+        <li>
+          <p>Protein shake</p>
+          <time>1:04pm</time>
+          <p>38g</p>
+          <button>delete</button>
+        </li>
+      </ul>
+    </div>
+  )
 }
