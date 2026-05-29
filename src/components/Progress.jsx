9dacfe8bacd1b9517573cf7e02c28e76a0849ed9
rumbawajomar <rumbawajomar@gmail.com>
2026-05-28 22:24:13 -0700

    Update Progress component

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,3 +1,25 @@
 export default function Progress() {
-  return <div>progress bar component goes here</div>
+  return (
+    <div>
+      <div>
+        <p>daily goal</p>
+        <p>10g/150g</p>
+      </div>
+      <div>progress bar...</div>
+      <div>
+        <div>
+          <p>57g</p>
+          <small>logged</small>
+        </div>
+        <div>
+          <p>63g</p>
+          <small>remaining</small>
+        </div>
+        <div>
+          <p>58%</p>
+          <small>complete</small>
+        </div>
+      </div>
+    </div>
+  )
 }
